e370ce0a4f948f83d9d4713d887fa2041a3a96e5
afincato <andre.not.me@gmail.com>
2017-06-30 13:10:13 +0200

    fork dw-gallery plugin

---
+++ lib/plugins/gallery/lang/it/settings.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Dmitry Baikov <dsbaikov@gmail.com>
+ * @author Diego Pierotto <ita.translations@tiscali.it>
+ * @author Antonio <antonioippolito@gmail.com>
+ */
+$lang['thumbnail_width']       = 'Larghezza immagine anteprima';
+$lang['thumbnail_height']      = 'Altezza immagine anteprima';
+$lang['image_width']           = 'Larghezza immagine';
+$lang['image_height']          = 'Altezza immagine';
+$lang['cols']                  = 'Immagini per riga';
+$lang['sort']                  = 'Come ordinare le immagini della galleria';
+$lang['sort_o_file']           = 'ordina per nome del file';
+$lang['sort_o_mod']            = 'ordina per data del file';
+$lang['sort_o_date']           = 'ordina per data dell\'EXIF';
+$lang['sort_o_title']          = 'ordina per titolo dell\'EXIF';
