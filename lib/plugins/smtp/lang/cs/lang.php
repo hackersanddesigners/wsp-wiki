21299a449e1e9fac7a9fa82ac6cfede814b86cbd
André Fincato <work@andrefincato.info>
2025-11-17 16:37:52 +0100

    remove unused langs from smtp plugin

--- lib/plugins/smtp/lang/cs/lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
- * @author Pavel Krupička <pajdacz@gmail.com>
- */
-$lang['menu']                  = 'Zkouška nastavení SMTP';
-$lang['nofrom']                = 'Nenastavili jste možnost \'mailfrom\'. Odesílání e-mailů pravděpodobně selže.';
